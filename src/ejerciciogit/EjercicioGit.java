48d39a267993bb8c199d33e82a9f2591542efde4
raul- <raul-@RAUL>
2020-12-15 09:39:34 +0100

    Líneas en el método main

--- src/ejerciciogit/EjercicioGit.java
+++ src/ejerciciogit/EjercicioGit.java
@@ -16,6 +16,8 @@ public class EjercicioGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+            System.out.println("Línea de visualización 1");
+            System.out.println("Línea de visualización 2");
     }
     
 }
